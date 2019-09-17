cec5bc5dd5d4d1be45f1a2a9365ea64652238921
Abk <travelkit.app@gmail.com>
2019-09-17 18:15:43 +0200

    Adding "Updating post"

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 
 const Post = require('../model/post');
@@ -17,28 +20,6 @@ exports.getPosts = (req, res, next) => {
       next(err);
     })
 };
-
-exports.getPost = (req, res, next) => {
-  const postId = req.params.postId;
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('Post not found');
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({
-        message: 'Post fetched',
-        post: post
-      });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    })
-}
 
 exports.createPost = (req, res, next) => {
   const validationErrors = validationResult(req);
@@ -78,3 +59,73 @@ exports.createPost = (req, res, next) => {
       next(err);
     })
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Post not found');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: 'Post fetched',
+        post: post
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+}
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    const error = new Error('Failed to create post. Invalid input.');
+    error.statusCode = 422;
+    throw error;
+  };
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\","/");
+  }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Post not found');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then(updatedPost => {
+      res.status(200).json({
+        message: "Post updated",
+        post: updatedPost
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+}
+
+const clearImage = relativeFilepath => {
+  const absoluteFilepath = path.join(__dirname, '..', relativeFilepath);
+  fs.unlink(absoluteFilepath, err => console.log(err));
+}
