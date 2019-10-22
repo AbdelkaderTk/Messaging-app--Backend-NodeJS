3e37cfe426f9ea83f6b4b2bfa640934930bb54c9
Abk <travelkit.app@gmail.com>
2019-10-22 18:18:44 +0200

    Using file and viewving single post details

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -49,6 +50,19 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated')
+  }
+  if (!req.file) {
+    return res.status(200).json({message: 'No file provided'});
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({message: "File stored", filePath: req.file.path})
+})
+
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
   rootValue: graphqlResolvers,
@@ -81,3 +95,8 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err))
+
+const clearImage = relativeFilepath => {
+  const absoluteFilepath = path.join(__dirname, '..', relativeFilepath);
+  fs.unlink(absoluteFilepath, err => console.log(err));
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -131,5 +131,24 @@ module.exports = {
       }),
       totalPosts: totalPosts
     }
+  },
+  post: async function({ id }, req) {
+    if (!req.isAuth) {
+      const error = new Error({message: 'Invalid user'});
+      error.statusCode = 401;
+      throw error
+    }
+    const post = await Post.findById(id).populate('creator');
+    if (!post) {
+      const error = new Error({message: 'Post not found'});
+      error.statusCode = 404;
+      throw error
+    }
+    return {
+      ...post._doc,
+      _id: post._id.toString(),
+      createdAt: post.createdAt.toISOString(),
+      updatedAt: post.updatedAt.toISOString()
+    }
   }
 }
